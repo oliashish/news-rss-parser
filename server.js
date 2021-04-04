81e095f38dcae57f9db8f36243451aac6d3a7118
Ashish oli <oliashish3@gmail.com>
2021-04-04 22:39:34 +0530

    package.json change

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use("/", routes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
 }
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, () => {
     console.log(`app listening to port : ${PORT}`);
 });

ce8afab819c78444cee9d309b1bc71dd5d1f3567
Ashish oli <oliashish3@gmail.com>
2021-04-04 22:52:43 +0530

    refracted: package.json and server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/", routes);
 
@@ -21,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
 });
 app.listen(PORT, () => {
     console.log(`app listening to port : ${PORT}`);
